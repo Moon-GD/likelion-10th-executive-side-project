8f468ff1cb12e463bf62bb0b618bbccec1d1e7a2
Moon GD_LG <king4mun@gmail.com>
2022-02-05 19:15:51 +0900

    길 찾기 게임 로직 완성

--- game.js
+++ game.js
@@ -69,6 +69,14 @@
     $('#REPEAT').click(()=>{
         let empty_number = isLoadEmpty()
         loadOn(empty_number, repeat_html)
+
+        // repeat은 한 번만 클릭할 수 있도록 설정
+        $('#LOOP-SPACE').css({
+            "background-color":"grey",
+            "color":"white",
+            "opacity":"0.3"
+        })
+        $('#REPEAT').off('click')
     })
 
     // 클릭 시 버튼 비활성화
@@ -106,20 +114,100 @@
             fifth = true
         }
     })
-    
+
+    // 사자 위치 정보
+    let x = 0, y = 0, flag = false
     let deg = 0
+    
+    let word_list = [
+        false, false, false, false, 
+        false, false, false
+    ]
+
     function go() {
+        // 글자 위치 정보
+
         if(deg == 0 || deg == 360) {
             $('#LION').animate({left:"+=5vw"}, 500)
+            x++
         }
         else if(deg == 90 || deg == -270) {
             $('#LION').animate({bottom:"-=5vw"}, 500)
+            y--
         }
         else if(deg == -90 || deg == 270) {
             $('#LION').animate({bottom:"+=5vw"}, 500)
+            y++
         }
         else if(deg == 180 || deg == -180) {
             $('#LION').animate({left:"-=5vw"}, 500)
+            x--
+        }
+
+        if(x==1 && y==0) {
+            setTimeout(()=>{
+                $('.first-letter').fadeOut()
+            }, 500)
+            word_list[0] = true
+        }
+        else if(x==1 && y==1) {
+            setTimeout(()=>{
+                $('.second-letter').fadeOut()
+            }, 500)
+            word_list[1] = true
+        }
+        else if(x==2 && y==1) {
+            setTimeout(()=>{
+                $('.third-letter').fadeOut()
+            }, 500)
+            word_list[2] = true
+        }
+        else if(x==2 && y==2) {
+            setTimeout(()=>{
+                $('.fourth-letter').fadeOut()
+            }, 500)
+            word_list[3] = true
+        }
+        else if(x==3 && y==2) {
+            setTimeout(()=>{
+                $('.fifth-letter').fadeOut()
+            }, 500)
+            word_list[4] = true
+        }
+        else if(x==3 && y==3) {
+            setTimeout(()=>{
+                $('.sixth-letter').fadeOut()
+            }, 500)
+            word_list[5] = true
+        }
+        else if(x==4 && y==3) {
+            setTimeout(()=>{
+                $('.seventh-letter').fadeOut()
+            }, 500)
+            word_list[6] = true
+        }
+        else {
+            setTimeout(()=>{
+                if(flag == false ) {
+                    alert('사자가 경로를 이탈하였습니다!')
+                    window.location.reload()
+                }
+            }, 600)
+        }
+
+        let correct = true
+        for(let i = 0 ; i < 7 ; i++) {
+            if(word_list[i] == false) {
+                correct = false
+                break
+            }
+        }
+
+        if(correct) {
+            setTimeout(()=>{
+                flag = true
+                $('.content').fadeOut()
+            }, 700)
         }
     }
 
@@ -149,156 +237,71 @@
     }
     
     let order = ['#FIRST', '#SECOND', '#THIRD', '#FOURTH', '#FIFTH']
-    let i = 0, infinite = 0
+
     
-    /*
-    function play(i) {
-        content = $(order[i]).html()
-        return new Promise((resolve, reject) => {
+    $('.go-btn').click(() => {
+        // let content = $(order[i]).html()
+        let repeat_exist = false
+        let p;
+        for(p=0;p<5;p++) {
+            let content = $(order[p]).html()
+
             if(content == repeat_html) {
-                i = 0
-                reject(i)
-            }
-            else if(content == go_html) {
-                resolve(go)
-            }
-            else if(content == east_html) {
-                resolve(east)
-            }
-            else if(content == north_html) {
-                resolve(north)
-            }
-            else if(i >= 5 || infinite > 20) {
-                window.location.reload()  // 일정 횟수가 넘어가면 페이지 리로드
-                reject(200)
-            }
-            else {
-                i++
-                reject(i)
-            }
-        })
-    }
-
-    $('.go-btn').click(() => {
-        play(i)
-        .then((state)=>{
-            state()
-            i = i + 1
-            infinite++
-        })
-        .catch((index)=>{
-            i = index, infinite++
-            if(i == 0) {
-                play(i)
-                .then((state)=>{
-                    state()
-                    i = i + 1
-                    infinite++
-                })
-                .catch((index)=>{
-                    i = index, infinite++
-                })
-            }
-        })
-    })
-    */
-    function delay(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms))
-    }
-
-    let go_sync = async function() {
-        if(deg == 0 || deg == 360) {
-            setTimeout(()=>{
-                $('#LION').animate({left:"+=5vw"}, 500)
-            }, 0)
-        }
-        else if(deg == 90 || deg == -270) {
-            setTimeout(()=>{
-                $('#LION').animate({bottom:"-=5vw"}, 500)
-            }, 0)
-        }
-        else if(deg == -90 || deg == 270) {
-            setTimeout(()=>{
-                $('#LION').animate({bottom:"+=5vw"}, 500)
-            }, 0)
-        }
-        else if(deg == 180 || deg == -180) {
-            setTimeout(()=>{
-                $('#LION').animate({left:"-=5vw"}, 500)
-            }, 1000)
-        }
-        await delay(1000)
-    }
-
-    let north_sync = async function() {
-        for(let i=0;i<90;i++)
-        {
-            setTimeout(()=>{
-                $('#LION').css({"transform":`rotate(${deg - i * 1}deg)`})
-            }, i * 10)
-        }
-        setTimeout(()=>{
-            deg -= 90
-        }, 950)
-
-        await delay(1000)
-    }
-
-    let east_sync = async function() {
-        for(let i=0;i<90;i++)
-        {
-            setTimeout(()=>{
-                $('#LION').css({"transform":`rotate(${deg + i * 1}deg)`})
-            }, i * 10)
-        }
-
-        setTimeout(()=>{
-            deg += 90
-        }, 950)
-
-        await delay(1000)
-    }
-
-    let repeat_sync = async function() {
-
-        await delay(1000)
-    }
-
-    let order_list = []
-    $('.go-btn').click(() => {
-        for(let i=0;i<3;i++) {
-            let content = $(order[i]).html()
-            if(content == go_html) {
-                order_list.push(go_sync())
-            }
-            else if(content == north_html) {
-                order_list.push(north_sync())
-            }
-            else if(content == east_html) {
-                order_list.push(east_sync())
-            }
-            else if(content == repeat_html) {
-                order_list.push(repeat_sync())
+                repeat_exist = true
+                break
             }
         }
-        
-        j = 0
-        for(let i=0;i<3;i+=2, j+=2) {
-            async function hi() {
-                order_list[j].then(()=> {
-                    if(j<3) {
-                        order_list[i+1]
+
+        // 경로에 반복 키워드가 존재하는 경우
+        if(repeat_exist) {
+            let loop_max = 60, loop_count = p + 1
+            let time = 0
+            for(let j=0;j<loop_max/loop_count;j++) {
+                for(let i=0;i<=p;i++) {
+                    setTimeout(()=>{
+                        content = $(order[i]).html()
+                        if(content == go_html) {
+                            go()
+                        }
+                        else if(content == north_html) {
+                            north()
+                        }
+                        else if(content == east_html) {
+                            east()
+                        }
+                    }, time + 1000 * i)
+                }
+                time += p * 1000
+            }
+        }
+        // 경로에 반복 키워드가 존재하지 않는 경우
+        else {
+            for(let i=0;i<5;i++) {
+                setTimeout(()=>{
+                    content = $(order[i]).html()
+                    if(content == go_html) {
+                        go()
                     }
-                    else if(order_list[j] == repeat_sync()){
-                        console.log(1)
+                    else if(content == north_html) {
+                        north()
                     }
-                })
+                    else if(content == east_html) {
+                        east()
+                    }
+                }, 1000 * i)
             }
-
-            hi()
+            setTimeout(()=>{
+                alert('아쉽지만 글씨를 모두 지나가지 못하였습니다!')
+                window.location.reload()
+            }, 5000)
         }
-        
     })
 
+    $('.reload-btn').click(()=>{
+        window.location.reload()
+    })
 
+    $('.pass').click(()=>{
+        $('.content').fadeOut()
+    })
 })()
